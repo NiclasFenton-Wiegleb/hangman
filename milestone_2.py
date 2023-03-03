45868ed2e3b4db375a82e91392d43a33c5b054d4
NiclasFenton-Wiegleb <126886146+NiclasFenton-Wiegleb@users.noreply.github.com>
2023-03-03 16:00:23 +0000

    Adding validation for user input to be one letter

--- milestone_2.py
+++ milestone_2.py
@@ -1,9 +1,14 @@
 import random
 
 word_list = ['Apple', 'Bannana', 'Mango', 'Pear', 'Grapes']
-#print(word_list)
+print(word_list)
 
 word = random.choice(word_list)
-#print(word)
+print(word)
 
 guess = input('Guess a single letter:\n')
+
+if len(guess) == 1 and guess.isalpha() == True:
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
