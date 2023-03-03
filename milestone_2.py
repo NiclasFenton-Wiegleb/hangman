885cde908189a52e72a90ceba9c5cd82ff9bad6d
NiclasFenton-Wiegleb <126886146+NiclasFenton-Wiegleb@users.noreply.github.com>
2023-03-03 15:49:27 +0000

    Adding 'guess' variable with user input.

--- milestone_2.py
+++ milestone_2.py
@@ -4,4 +4,6 @@ word_list = ['Apple', 'Bannana', 'Mango', 'Pear', 'Grapes']
 #print(word_list)
 
 word = random.choice(word_list)
-print(word)
+#print(word)
+
+guess = input('Guess a single letter:\n')
