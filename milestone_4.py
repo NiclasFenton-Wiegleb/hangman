494b74913d9dbc27654d39fe5fc5a649f4484dfe
NiclasFenton-Wiegleb <126886146+NiclasFenton-Wiegleb@users.noreply.github.com>
2023-03-04 10:52:06 +0000

    Updating hangman class to Hangman

--- milestone_4.py
+++ milestone_4.py
@@ -4,18 +4,15 @@ word_list = ['apple', 'banana', 'orange', 'lemon', 'lime', 'melon', 'watermelon'
                           'kiwi', 'peach', 'avocado', 'coconut', 'apricot', 'blueberry', 'blackberry', 'strawberry', 'raspberry', 'cherry', 'pear', 
                           'mandarin', 'durian', 'passion fruit', 'persimmon', 'dragon fruit', 'papaya', 'pomegranate']
 
-class hangman:
+class Hangman:
     def __init__(self,word_list, num_lives) -> None:
         self.word = random.choice(word_list)
+        self.list_of_guesses = []
         self.word_guessed = [letter if letter in self.list_of_guesses else '_' for letter in self.word]
         self.num_letters = len(set([letter for letter in self.word if letter not in self.list_of_guesses]))
         self.num_lives = 5
         self.word_list = word_list
-        self.list_of_guesses = []
         
-        
-        
-
-x = hangman(word_list,5)
+x = Hangman(word_list,5)
 
 print(x.word)
