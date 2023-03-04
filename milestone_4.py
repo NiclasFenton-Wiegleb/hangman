b3024a372aa64781a31da734cdf6c6dcbba19328
NiclasFenton-Wiegleb <126886146+NiclasFenton-Wiegleb@users.noreply.github.com>
2023-03-04 12:38:31 +0000

    Fixing word_guessed list and updating check_guess

--- milestone_4.py
+++ milestone_4.py
@@ -1,14 +1,16 @@
 import random
 
 fruit_lst = ['apple', 'banana', 'orange', 'lemon', 'lime', 'melon', 'watermelon', 'grape', 'grapefruit', 'plum', 'mango', 'pineapple', 
-                          'kiwi', 'peach', 'avocado', 'coconut', 'apricot', 'blueberry', 'blackberry', 'strawberry', 'raspberry', 'cherry', 'pear', 
-                          'mandarin', 'durian', 'passion fruit', 'persimmon', 'dragon fruit', 'papaya', 'pomegranate']
+                'kiwi', 'peach', 'avocado', 'coconut', 'apricot', 'blueberry', 'blackberry', 'strawberry', 'raspberry', 'cherry', 'pear', 
+                'mandarin', 'durian', 'passion fruit', 'persimmon', 'papaya', 'pomegranate']
 
 class Hangman:
     def __init__(self,word_list, num_lives = 5) -> None:
         self.word = random.choice(word_list)
         self.list_of_guesses = []
-        self.word_guessed = [letter if letter in self.list_of_guesses else '_' for letter in self.word]
+        self.word_guessed = []
+        for letter in self.word:
+                self.word_guessed.append('_')
         self.num_letters = len(set([letter for letter in self.word if letter not in self.list_of_guesses]))
         self.num_lives = num_lives
         self.word_list = word_list
@@ -17,6 +19,12 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print("Good guess! "+guess+" is in the word.")
+            indices = []
+            for letter in range(len(self.word)):
+                if self.word[letter] == guess:
+                    self.word_guessed[letter] = guess
+        self.num_letters = self.num_letters - 1
+
             
     def ask_for_input(self):
         while True:
@@ -32,4 +40,6 @@ class Hangman:
 
 x = Hangman(fruit_lst)
 
-print(x.num_lives)
+x.ask_for_input()
+print(x.word_guessed)
+
